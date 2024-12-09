cd53bde94756b59bc01e5c08a15ce7bcc4e9b836
tijmenstor <23655598+tijmenstor@users.noreply.github.com>
2024-12-09 13:55:20 +0100

    fix: Fight the format

--- src/mpyl/steps/deploy/k8s/helm.py
+++ src/mpyl/steps/deploy/k8s/helm.py
@@ -30,11 +30,13 @@ def template_chart(
     values_path: Path,
     output_path: Path,
 ) -> Output:
-    cmd = f"helm template {release_name} " \
-        f"{chart_name} " \
-        f"--version {chart_version} " \
-        f"-f {values_path} " \
+    cmd = (
+        f"helm template {release_name} "
+        f"{chart_name} "
+        f"--version {chart_version} "
+        f"-f {values_path} "
         f"--output-dir {output_path}"
+    )
     return custom_check_output(logger, cmd)
 
 
